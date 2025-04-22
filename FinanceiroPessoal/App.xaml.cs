07eb7a13424579f8b8251eafcc6ef61adecd75dd
renatoads1 <renato.lacerda>
2025-04-22 11:04:52 -0300

    Adicionar arquivos de projeto.

---
+++ FinanceiroPessoal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceiroPessoal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
